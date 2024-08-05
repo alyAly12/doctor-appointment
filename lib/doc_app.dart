376710fca3b8170a2d7324ff7802ee49f5855738
mohamed <medoaly870@gmail.com>
2024-08-05 23:16:05 +0300

    done with splash screen

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,9 +1,9 @@
 import 'package:doctor_appointment/core/routing/app_router.dart';
-import 'package:doctor_appointment/core/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/routes.dart';
+import 'core/utils/app_colors.dart';
 
 class DocApp extends StatelessWidget {
   const DocApp({super.key, required this.appRouter});
@@ -11,7 +11,7 @@ final AppRouter appRouter;
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       child:  MaterialApp(
