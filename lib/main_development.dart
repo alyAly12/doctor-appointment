3579466c2a697bb2190a78219773484db37734b4
mohamed <medoaly870@gmail.com>
2024-08-12 22:01:45 +0300

    fixing screen util text issue in release mode

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,11 @@ import 'package:doctor_appointment/core/di/dependency_injection.dart';
 import 'package:doctor_appointment/core/routing/app_router.dart';
 import 'package:doctor_appointment/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   await initGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
