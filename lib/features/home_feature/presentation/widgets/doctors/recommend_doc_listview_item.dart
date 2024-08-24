ff383062f5eecc6b9c200c6a0235464c9f7e4ac8
mohamed <medoaly870@gmail.com>
2024-08-24 17:33:19 +0300

    adding cached network image and improving shimmer effect

--- lib/features/home_feature/presentation/widgets/doctors/recommend_doc_listview_item.dart
+++ lib/features/home_feature/presentation/widgets/doctors/recommend_doc_listview_item.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:doctor_appointment/features/home_feature/data/models/specialization_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/common_widgets/custom_text_widget.dart';
 import '../../../../../core/utils/app_colors.dart';
@@ -14,15 +16,45 @@ final Doctors? doctorModel;
       margin: EdgeInsets.only(bottom: 16.h),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              'https://media.istockphoto.com/id/1922549572/photo/portrait-of-young-female-doctor-thinking.jpg?s=1024x1024&w=is&k=20&c=4lviwdg384zsZXU2J7Eex3G29PcoApNb9FjeDQEWlQo=',
+          CachedNetworkImage(
+            imageUrl: 'https://media.istockphoto.com/id/1922549572/photo/portrait-of-young-female-doctor-thinking.jpg?s=1024x1024&w=is&k=20&c=4lviwdg384zsZXU2J7Eex3G29PcoApNb9FjeDQEWlQo=',
+          progressIndicatorBuilder: (context, url, downloadProgress) {
+            return Shimmer.fromColors(
+                baseColor: AppColors.lightTitleColor,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.white
+                  ),
+                )
+            );
+          },
+            imageBuilder:  (context, imageProvider) => Container(
               width: 110.w,
               height: 120.h,
-              fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(12),
+          //   child: Image.network(
+          //     'https://media.istockphoto.com/id/1922549572/photo/portrait-of-young-female-doctor-thinking.jpg?s=1024x1024&w=is&k=20&c=4lviwdg384zsZXU2J7Eex3G29PcoApNb9FjeDQEWlQo=',
+          //     width: 110.w,
+          //     height: 120.h,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
           SizedBox(
             width: 16.w,
           ),
